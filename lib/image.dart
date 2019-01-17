be134ee2b837475bc1df1c459bf5ace6ec5b3957
xuefengyang <yangxuefengy@gmail.com>
2019-01-17 23:50:38 +0800

    Add Image

---
+++ lib/image.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ImageContainer extends Container {
+  
+  // TODO image implement
+  @override
+    Widget build(BuildContext context) {
+      return super.build(context);
+    }
+}
