69df69cc455d236a94ded6d74503ce701156b319
xuefengyang <yangxuefengy@gmail.com>
2019-01-21 23:55:52 +0800

    Add Button

--- lib/login/index_page.dart
+++ lib/login/index_page.dart
@@ -9,16 +9,42 @@ class LoginIndexPage extends StatelessWidget {
       color: Color.fromARGB(100, 51, 55, 74),
       child: Column(
         children: <Widget>[
-          Container(
-            margin: EdgeInsets.all(40.0),
-            child: Row(
+          Expanded(
+            flex:53,
+            child: Container(
+              margin: EdgeInsets.all(40.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Image(image: null)
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 47,
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                Image(image: null)
+                InkWell(
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(30, 0, 30, 0),
+                    height: 44.0,
+                    child: Text("登录"),
+                  ),
+                ),
+                InkWell(
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(30, 8, 30, 0),
+                    height: 44.0,
+                    child: Text("注册"),
+                  ),
+                )
               ],
             ),
-          )
+          ),
         ],
       ),
     );
